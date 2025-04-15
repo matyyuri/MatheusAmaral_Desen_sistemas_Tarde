f206911768e1901d94fb46a50fa70d9e1dd77c9d
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-15 17:06:41 -0300

    Ex Array

---
+++ MatheusAmaral (php)/ExemploArray.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <?php
+            $dias = array('domingo','segunda','terça','quarta','quinta','sexta','sábado');
+            echo $dias[1]."<br>";
+            print_r($dias);
+            echo "<br>";
+            var_dump($dias);
+        ?>
+
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </body>
+</html>
