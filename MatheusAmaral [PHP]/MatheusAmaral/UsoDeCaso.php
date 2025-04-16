faffb89f744af929f1e243eb6881e09d739f917c
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-16 16:47:38 -0300

    Uso de caso php

---
+++ MatheusAmaral [PHP]/MatheusAmaral/UsoDeCaso.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <?php
+            $s = "lampada";
+            switch ($s) {
+                case "casa":
+                    print "A casa é amarela";
+                    break;
+                
+                case "arvore":
+                    print "a árvpre é bonita";
+                    break;
+                
+                case "lampada":
+                    print "joão apagou a lâmpada";
+                    break;
+
+                default:
+                    print "você não selecionou";
+                    break;
+            }
+        ?>
+        
+        <?php echo"<br><br><br>"?>
+        
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </body>
+</html>
