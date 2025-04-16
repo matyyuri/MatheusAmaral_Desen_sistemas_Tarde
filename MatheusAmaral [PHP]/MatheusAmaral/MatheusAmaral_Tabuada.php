db118d414e4b753d8625efe43764800fa9e2fc7b
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-16 16:48:03 -0300

    Tabuada php

---
+++ MatheusAmaral [PHP]/MatheusAmaral/MatheusAmaral_Tabuada.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela PHP</title>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+        }
+        th, td {
+            border-style: solid;
+            width: 50px;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <?php
+            for ($l=1;$l<=10;$l++)
+            {
+                echo "<tr>";
+                for ($c=1; $c<=10;$c++)
+                {
+                    $resultado = $l * $c;
+                    echo "<td> $l x $c = $resultado </td>";
+                }
+                echo "<tr/>";
+            }
+        ?>
+        
+        <?php echo"<br><br><br>"?>
+
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </table>
+</body>
+</html>
