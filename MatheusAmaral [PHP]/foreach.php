54b3b4272671bfc3e59041f3371ca60eb9dd105f
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-16 16:49:01 -0300

    foreach php

---
+++ MatheusAmaral [PHP]/foreach.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <?php
+           $cores = array("Amarelo","Vermelho","Verde","Azul");
+           foreach($cores as $cor) {
+                 echo "". $cor ."<br>";
+           }
+        ?>
+        
+        <?php echo"<br><br><br>"?>
+        
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </body>
+</html>
