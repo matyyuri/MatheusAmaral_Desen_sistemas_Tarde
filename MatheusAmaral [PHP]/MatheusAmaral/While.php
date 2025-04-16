e12744fc8316728ff9c557fc37884653cefccdde
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-16 16:47:31 -0300

    While php

---
+++ MatheusAmaral [PHP]/MatheusAmaral/While.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <?php
+            $i = 1;
+            while ($i <= 10) {
+                echo $i;
+                $i++;
+            }
+        ?>
+        
+        <?php echo"<br><br><br>"?>
+        
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </body>
+</html>
