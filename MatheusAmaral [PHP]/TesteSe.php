2d12bab107de0747644bb6d2333e9f7055c42a37
Matheus Yuri do Amaral <yuriamaral096@gmail.com>
2025-04-16 16:47:01 -0300

    Teste Se

---
+++ MatheusAmaral [PHP]/TesteSe.php
@@ -0,0 +1,23 @@
+<html>
+    <body>
+        <?php
+            //Declara variável numérica
+            $umidade = 91;
+            //Testa se $umidade maior que 90. Retorna um boolean
+            $vaiChover = ($umidade > 90);
+            //Testa se $vaiChover é verdadeiro.
+            if ($vaiChover)
+            {
+                echo "Vai chover com toda certeza absoluta da terra!";
+            }
+        ?>
+        
+        <?php echo"<br><br><br>"?>
+        
+        <?php
+            echo " <h2 align='center'>
+                  Aluno: Matheus Yuri do Amaral
+                  </h2>";
+        ?>
+    </body>
+</html>
